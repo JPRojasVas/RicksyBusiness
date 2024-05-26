dba3b2c0e3b75a601a0330219611cd5cfc5d5115
JPRojasVas <Juanparovas3@gmail.com>
2024-05-26 18:50:37 +0200

    feat (CreditCard.java) : create class CreditCard and add methods

---
+++ src/main/java/edu/badpals/ricksy/CreditCard.java
@@ -0,0 +1,41 @@
+package edu.badpals.ricksy;
+
+public class CreditCard {
+    private final String number;
+    private double credit;
+    private final String owner;
+    private final String SYMBOL = "1";
+
+    CreditCard(String owner, String number) {
+        this.number = number;
+        this.owner = owner;
+        this.credit = 3000;
+    }
+
+    boolean pay(double pago){
+        if (this.credit() >= pago){
+            this.credit -= pago;
+            return true;
+        }
+        return false;
+    }
+
+    String number() {
+        return number;
+    }
+
+    double credit() {
+        return credit;
+    }
+
+    String getOwner() {
+        return owner;
+    }
+
+    @Override
+    public String toString() {
+        return "owner: " + this.getOwner() + "\n" +
+                "number: " + this.number() + "\n" +
+                "credit: " + this.credit() + "EZI";
+    }
+}
