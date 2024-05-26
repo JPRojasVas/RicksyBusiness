5a43d56f0e22f8dcc044a5b4d7374e9f89e54fce
JPRojasVas <Juanparovas3@gmail.com>
2024-05-26 18:52:17 +0200

    feat (UfosPark.java) : create class UfosPark and add methods

---
+++ src/main/java/edu/badpals/ricksy/UfosPark.java
@@ -0,0 +1,55 @@
+package edu.badpals.ricksy;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class UfosPark implements GuestDispatcher {
+    private static final String LIBRE = "libre";
+    private final Map<String, String> flota = new HashMap<>();
+    private final double fee = 500;
+
+    UfosPark() {
+    }
+
+    void add(String ufo) {
+        flota.putIfAbsent(ufo, LIBRE);
+    }
+
+    @Override
+    public void dispatch(CreditCard tarjeta) {
+        if (this.getUfoOf(tarjeta.number()) != null) {
+            return;
+        }
+        Map<String, String> ufosLibres = this.flota.entrySet()
+                .stream()
+                .filter((Map.Entry<String, String> entry) -> entry.getValue().equals(LIBRE))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        if (!ufosLibres.isEmpty() && tarjeta.pay(this.fee)) {
+            String ufoLibre = (String)ufosLibres.keySet()
+                    .toArray()[new Random().nextInt(0, ufosLibres.size())];
+            flota.replace(ufoLibre, tarjeta.number());
+        }
+    }
+
+    String getUfoOf(String number) {
+        return this.flota.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().equals(number))
+                .map(Map.Entry::getKey)
+                .findFirst().orElse(null);
+
+    }
+
+    @Override
+    public String toString() {
+        return flota.keySet().stream().sorted().toList().toString();
+    }
+
+    Collection<String> cardNumbers() {
+        return this.flota.values();
+    }
+
+    boolean containsCard(String cardNumber) {
+        return this.flota.containsValue(cardNumber);
+    }
+}
